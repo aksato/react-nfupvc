820a93bda4fc83037206f95386d9f302a958e8c9
aksato <andresato@hotmail.com>
2022-11-18 19:43:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nfupvc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nfupvc)
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+import logo from './logo.svg';
+
+export default function App() {
+  return (
+    <div>
+      <header id="cabecalho-principal">
+        <a href="#">
+          {' '}
+          <img src={logo} />{' '}
+        </a>
+        <nav id="navegacao-principal">
+          <ul>
+            <li>
+              <a href="#">Filmes</a>
+            </li>
+            <li>
+              <a href="#">Listas</a>
+            </li>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main id="conteiner-principal">
+        <h2>Hello World</h2>
+      </main>
+      <footer id="rodape-principal">
+        <p>
+          <small>Copyright 2021</small>
+        </p>
+      </footer>
+    </div>
+  );
+}
